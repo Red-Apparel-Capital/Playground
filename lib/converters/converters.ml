918b91cde963ca59a6f98e9f786fe3d07b427be4
David <idajilid@gmail.com>
2024-12-15 13:32:06 +0000

    refac

--- lib/converters/converters.ml
+++ lib/converters/converters.ml
@@ -1,19 +1,19 @@
-let parse_date date_str =
-  match String.split_on_char '/' date_str with
+let parse_date (date : string) : int * int * int =
+  match String.split_on_char '/' date with
   | [ day; month; year ] ->
       (int_of_string year, int_of_string month, int_of_string day)
-  | _ -> failwith (Printf.sprintf "Invalid date format %s" date_str)
+  | _ -> failwith (Printf.sprintf "Invalid date format %s" date)
 
-let parse_time time_str =
-  match String.split_on_char ':' time_str with
+let parse_time (time : string) : int * int * int =
+  match String.split_on_char ':' time with
   | [ hour; minute; second ] ->
       (int_of_string hour, int_of_string minute, int_of_string second)
-  | _ -> failwith (Printf.sprintf "Invalid time format %s" time_str)
+  | _ -> failwith (Printf.sprintf "Invalid time format %s" time)
 
-let convert_to_timedesc datetime_str : Timedesc.t =
-  match String.split_on_char ' ' datetime_str with
+let convert_to_timedesc (datetime : string) : Timedesc.t =
+  match String.split_on_char ' ' datetime with
   | [ date; time ] ->
       let year, month, day = parse_date date in
       let hour, minute, second = parse_time time in
       Timedesc.make_exn ~year ~month ~day ~hour ~minute ~second ()
-  | _ -> failwith (Printf.sprintf "Invalid datetime format %s" datetime_str)
+  | _ -> failwith (Printf.sprintf "Invalid datetime format %s" datetime)
